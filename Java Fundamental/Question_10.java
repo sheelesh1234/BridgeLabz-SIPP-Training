3420464ea441dfa390f9b5a7bb397e340c0e05b5
Sheelesh mishra <Sheeleshmishra8@gmail.com>
2025-06-23 12:26:32 +0530

    [Sheelesh Mishra].Fix Deleted Other Directories

--- Java Fundamental/Question_10.java
+++
@@ -1,9 +0,0 @@
-// Create a program to convert the distance of 10.8 kilometers to miles.
-
-public class Question_10 {
-    public static void main(String[] args) {
-        double distanceInKm = 10.8;
-        double distanceInMiles = distanceInKm * 1.609;
-        System.out.println("The distance " + distanceInKm + " km in miles is " + distanceInMiles);
-    }
-}
